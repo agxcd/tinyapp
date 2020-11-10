58a51ff63133cfc6622ac6eecc83bd45a2680011
Your Name <your@email.com>
2020-11-10 05:51:17 +0000

    call make the short urls and redirect to the page

--- express_server.js
+++ express_server.js
@@ -25,8 +25,10 @@ app.get("/urls", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
+  res.send("Ok");
 });
 
 app.get("/urls/new", (req, res) => {
@@ -39,6 +41,11 @@ app.get("/urls/:shortURL", (req, res) => {
     longURL: urlDatabase[req.params.shortURL],
   };
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.get("/hello", (req, res) => {
